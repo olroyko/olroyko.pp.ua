b249789bb336c63e9d169a1ce3c9a3144f6e8f51
Oleg Royko <olroyko@gmail.com>
2017-11-25 15:12:00 +0200

    Add extra chapters; update content

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,8 +55,8 @@ jQuery(document).ready(function($) {
     );
 
 // Key facts
-//    CLIENTS ONFO
-    var clients_info = new ProgressBar.Circle(clients, {
+//    CLIENTS INFO CIRCLE
+    var clients_info = new ProgressBar.Circle('#clients', {
         strokeWidth: 8,
         easing: 'easeInOut',
         duration: 1800,
@@ -66,7 +66,7 @@ jQuery(document).ready(function($) {
         text: {
             // Initial value for text.
             // Default: null
-            value: '30 projects',
+            value: '> 40 clients',
             // Class name for text element.
             // Default: 'progressbar-text'
             className: 'progressbar__label',
@@ -82,7 +82,7 @@ jQuery(document).ready(function($) {
             style: {
                 // Text color.
                 // Default: same as stroke color (options.color)
-                color: '#f00',
+                color: '#778492',
                 position: 'absolute',
                 left: '50%',
                 top: '50%',
@@ -111,21 +111,147 @@ jQuery(document).ready(function($) {
         svgStyle: null
     });
 
-    window.onscroll = function() {displayAnimation()}; //function to display animation after scrolling down to 500px
-    function displayAnimation() {
-        if (document.body.scrollTop > 500) {
-            window.onscroll = null;
-            clients_info.animate(1.0).start();
+  //function to display animation after scrolling down to 500px
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            clients_info.animate(1.0);
         }
-    }
+    });
 
-    
+    //    PROJECTS INFO CIRCLE
+    var projects_info = new ProgressBar.Circle('#projects', {
+        strokeWidth: 8,
+        easing:      'easeInOut',
+        duration:    1800,
+        color:       '#c28dcb',
+        trailColor:  '#eee',
+        trailWidth:  1,
+        text:        {
+            // Initial value for text.
+            // Default: null
+            value: '> 60 projects',
+            // Class name for text element.
+            // Default: 'progressbar-text'
+            className: 'progressbar__label',
+            // Inline CSS styles for the text element.
+            // If you want to modify all CSS your self, set null to disable
+            // all default styles.
+            // If the style option contains values, container is automatically
+            // set to position: relative. You can disable behavior this with
+            // autoStyleContainer: false
+            // If you specify anything in this object, none of the default styles
+            // apply
+            // Default: object speficied below
+            style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#778492',
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                padding: 0,
+                margin: 0,
+                fontsize: 20,
+                // You can specify styles which will be browser prefixed
+                transform: {
+                    prefix: true,
+                    value: 'translate(-50%, -50%)'
+                }
+            },
+
+            // Only effective if the text.style is not null
+            // By default position: relative is applied to container if text
+            // is set. Setting this to false disables that feature.
+            autoStyleContainer: true,
+
+            // Only effective if the shape is SemiCircle.
+            // If true, baseline for text is aligned with bottom of
+            // the SVG canvas. If false, bottom line of SVG canvas
+            // is in the center of text.
+            // Default: true
+            alignToBottom: true
+        },
+        svgStyle: null
+    });
+
+    //function to display animation after scrolling down to 500px
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            projects_info.animate(1.0);
+        }
+    });
+
+    //    HOURS INFO CIRCLE
+    var hours_info = new ProgressBar.Circle('#hours', {
+        strokeWidth: 8,
+        easing:      'easeInOut',
+        duration:    1800,
+        color:       '#2db0cb',
+        trailColor:  '#eee',
+        trailWidth:  1,
+        text:        {
+            // Initial value for text.
+            // Default: null
+            value: '> 10000 hours',
+            // Class name for text element.
+            // Default: 'progressbar-text'
+            className: 'progressbar__label',
+            // Inline CSS styles for the text element.
+            // If you want to modify all CSS your self, set null to disable
+            // all default styles.
+            // If the style option contains values, container is automatically
+            // set to position: relative. You can disable behavior this with
+            // autoStyleContainer: false
+            // If you specify anything in this object, none of the default styles
+            // apply
+            // Default: object speficied below
+            style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#778492',
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                padding: 0,
+                margin: 0,
+                fontsize: 20,
+                // You can specify styles which will be browser prefixed
+                transform: {
+                    prefix: true,
+                    value: 'translate(-50%, -50%)'
+                }
+            },
+
+            // Only effective if the text.style is not null
+            // By default position: relative is applied to container if text
+            // is set. Setting this to false disables that feature.
+            autoStyleContainer: true,
+
+            // Only effective if the shape is SemiCircle.
+            // If true, baseline for text is aligned with bottom of
+            // the SVG canvas. If false, bottom line of SVG canvas
+            // is in the center of text.
+            // Default: true
+            alignToBottom: true
+        },
+        svgStyle: null
+    });
+
+    //function to display animation after scrolling down to 500px
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            hours_info.animate(1.0);
+        }
+    });
+
+
+
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "olroyko");
+    // GitHubCalendar("#github-graph", "olroyko");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "olroyko", selector: "#ghfeed" });
+    // GitHubActivity.feed({ username: "olroyko", selector: "#ghfeed" });
 
 
 });
