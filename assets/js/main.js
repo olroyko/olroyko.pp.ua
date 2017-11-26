1604335aeec128c43d781e49ba99d44b2e042b58
Oleg Royko <olroyko@gmail.com>
2017-11-26 12:12:06 +0200

    Add  achievement chapters; moved documentation chapter; add CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,11 +247,11 @@ jQuery(document).ready(function($) {
 
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    // GitHubCalendar("#github-graph", "olroyko");
+    GitHubCalendar("#github-graph", "olroyko");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    // GitHubActivity.feed({ username: "olroyko", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "olroyko", selector: "#ghfeed" });
 
 
 });
